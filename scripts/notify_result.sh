35a1ba162ef1f76170bc9aebea8a56b2b11c0d42
yoRyuuuuu <sasoseooo@gmail.com>
2024-11-12 23:38:52 +0900

    add TODO to scripts/notify_result.sh

--- scripts/notify_result.sh
+++ scripts/notify_result.sh
@@ -2,16 +2,19 @@
 
 set -eux
 
-REPO="yoRyuuuuu/private-isu-002"
+# TODO:リポジトリ名を変更する
+REPO="yoRyuuuuu/private-isu-001"
 TITLE="$(date '+%Y-%m-%d %H:%M:%S')の計測結果"
 ISSUE_URL=$(gh issue create --repo $REPO --title "$TITLE" --body "")
 
+# TODO: alpのオプションに、適切なURI matching groupsを設定する
+# https://github.com/tkuchiki/alp/blob/main/README.ja.md#uri-matching-groups
+# 例: -m '/@\w+,/image/*,/posts/[0-9]+'
 {
   echo "alp:"
   echo "\`\`\`"
   sudo cat /var/log/nginx/access.log | alp json \
     --sort sum -r \
-    -m '/@\w+,/image/*,/posts/[0-9]+' \
     -o count,method,1xx,2xx,3xx,4xx,5xx,uri,min,max,sum,avg
   echo "\`\`\`"
 } > /tmp/alp
