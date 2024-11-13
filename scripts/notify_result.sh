88145d0f068bfc6666245ab6f090ce64bc563c85
yoRyuuuuu <ryugo.yoshioka@fuller.co.jp>
2024-11-13 17:55:00 +0900

    計測結果の通知先をSlackに変更

--- scripts/notify_result.sh
+++ scripts/notify_result.sh
@@ -2,28 +2,27 @@
 
 set -eux
 
-# TODO:リポジトリ名を変更する
-REPO="yoRyuuuuu/private-isu-001"
-TITLE="$(date '+%Y-%m-%d %H:%M:%S')の計測結果"
-ISSUE_URL=$(gh issue create --repo $REPO --title "$TITLE" --body "")
+# TODO: SlackのWebhook URLを設定する
+# export SLACK_WEBHOOK_URL=
 
 # TODO: alpのオプションに、適切なURI matching groupsを設定する
 # https://github.com/tkuchiki/alp/blob/main/README.ja.md#uri-matching-groups
 # 例: -m '/@\w+,/image/*,/posts/[0-9]+'
+
+# alpの統計結果をSlackに通知する
 {
-  echo "alp:"
   echo "\`\`\`"
   sudo cat /var/log/nginx/access.log | alp json \
     --sort sum -r \
     -o count,method,1xx,2xx,3xx,4xx,5xx,uri,min,max,sum,avg
   echo "\`\`\`"
 } > /tmp/alp
-gh issue comment "$ISSUE_URL" --body-file /tmp/alp
+slm < /tmp/alp
 
+# pt-query-digestの統計結果をSlackに通知する
 {
-  echo "pt-query-digest:"
   echo "\`\`\`"
-  sudo pt-query-digest /var/log/mysql/mysql-slow.log | head -n 200
+  sudo pt-query-digest /var/log/mysql/mysql-slow.log | head -n 300
   echo "\`\`\`"
 } > /tmp/pt-query-digest
-gh issue comment "$ISSUE_URL" --body-file /tmp/pt-query-digest
+slm < /tmp/pt-query-digest
